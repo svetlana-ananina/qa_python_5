c62fa18e96ca5f9aa6a2853d8139bdaa0e2cb6d0
Svetlana Ananina <weta2809@yandex.ru>
2023-11-12 13:37:31 +0300

    Corrected comments

--- tests/test_registration_page.py
+++ tests/test_registration_page.py
@@ -11,23 +11,19 @@ class TestRegistrationPage:
         ''' Проверка успешной регистрации '''
         driver = open_reg_window  # Функция-фикстура открывает окно веб-драйвера и страницу регистрации
 
-        # Получаем данные для регистрации
-        name = get_user_name
-        login = gen_login  # Генерирует логин по шаблону
-        password = gen_password  # Генерирует пароль
+        name = get_user_name                                        # Получаем данные для регистрации
+        login = gen_login                                           # Генерирует логин по шаблону
+        password = gen_password                                     # Генерирует пароль
 
-        # Ждем появления кнопки 'Зарегистрироваться'
-        helpers.wait_element(driver, loc.REG_BUTTON)
+        helpers.wait_element(driver, loc.REG_BUTTON)                # Ждем появления кнопки 'Зарегистрироваться'
 
-        # Вводим данные в поля и кликаем кнопку
-        helpers.set_value(driver, loc.USER_NAME_INPUT, name)
+        helpers.set_value(driver, loc.USER_NAME_INPUT, name)        # Вводим данные в поля и кликаем кнопку
         helpers.set_value(driver, loc.USER_EMAIL_INPUT, login)
         helpers.set_value(driver, loc.USER_PASSWORD_INPUT, password)
 
         helpers.click_element(driver, loc.REG_BUTTON)
 
-        # Проверяем, что произошел переход на страницу авторизации
-        helpers.wait_url_changes(driver, url.REG_PAGE_URL)
+        helpers.wait_url_changes(driver, url.REG_PAGE_URL)          # Проверяем, что произошел переход на страницу авторизации
         assert driver.current_url == url.AUTH_PAGE_URL
 
         driver.quit()
@@ -36,26 +32,21 @@ class TestRegistrationPage:
                                                          gen_login, get_user_name, gen_invalid_password):
         ''' Проверка сообщения о некорректном пароле в форме регистрации '''
 
-        # Открываем страницу регистрации по прямой ссылке и ждем появления формы регистрации
-        driver = open_reg_window  # Функция-фикстура открывает окно веб-драйвера и страницу регистрации
+        driver = open_reg_window                                    # Открываем страницу регистрации и ждем появления формы регистрации
 
-        # Получаем данные для регистрации
-        name = get_user_name
-        login = gen_login  # Генерирует логин по шаблону
-        password = gen_invalid_password  # Генерирует пароль
+        name = get_user_name                                        # Получаем данные для регистрации
+        login = gen_login                                           # Генерирует логин по шаблону
+        password = gen_invalid_password                             # Генерирует пароль
 
-        # Ждем появления кнопки 'Зарегистрироваться'
-        helpers.wait_element(driver, loc.REG_BUTTON)
+        helpers.wait_element(driver, loc.REG_BUTTON)                # Ждем появления кнопки 'Зарегистрироваться'
 
-        # Вводим данные в поля и кликаем кнопку
-        helpers.set_value(driver, loc.USER_NAME_INPUT, name)
+        helpers.set_value(driver, loc.USER_NAME_INPUT, name)        # Вводим данные в поля и кликаем кнопку
         helpers.set_value(driver, loc.USER_EMAIL_INPUT, login)
         helpers.set_value(driver, loc.USER_PASSWORD_INPUT, password)
 
         helpers.click_element(driver, loc.REG_BUTTON)
 
-        # Ждем пока не появится сообщение об ошибке
-        helpers.wait_element(driver, loc.REG_PAGE_ERROR_MESSAGE)
+        helpers.wait_element(driver, loc.REG_PAGE_ERROR_MESSAGE)    # Ждем пока не появится сообщение об ошибке
         elements = helpers.find_elements(driver, loc.REG_PAGE_ERROR_MESSAGE)
         assert len(elements) == 1 and elements[0].text == data.DATA.INVALID_PASS_TEXT
 
